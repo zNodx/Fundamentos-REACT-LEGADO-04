feac97aa867326cde6c3b864cb1b35dccf5a7947
mateus <ffrmateus@gmail.com>
2022-01-21 13:53:54 -0300

    Criando o componente familia e passando os props declarado para o pai pros filhos

---
+++ src/components/FamiliaLima.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Membro from './Membro'
+export default props => (
+    <div>
+        <Membro nome="Marcos" sobreNome="Lima"/>
+        <Membro nome="Mateus" sobreNome="Lima"/>
+        <Membro nome="Maria" sobreNome="Lima"/>
+        <Membro nome="Felipe" sobreNome="Lima"/>
+    </div>
+)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MultiElementos from "./components/MultiElementos";
-
+import Familia from "./components/Familia";
+import Membro from "./components/Membro";
 const elemento = document.getElementById('root')
 const jsx = <h1>Olá React!</h1>
 ReactDOM.render(
     <div>
-      <MultiElementos/>
+        <h1>Familia Lima</h1>
+        <Familia sobreNome="Lima">
+            <Membro nome="Marcos"/>
+            <Membro nome="Mateus"/>
+            <Membro nome="Maria"/>
+            <Membro nome="Felipe"/>
+        </Familia>
     </div>
     ,elemento)
