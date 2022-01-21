413574f3d82319cb81cad3ae815c545ae0899b56
mateus <ffrmateus@gmail.com>
2022-01-21 14:20:09 -0300

    Comunicaçao direta

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MultiElementos from "./components/MultiElementos";
+import Funcao from "./components/ComponenteComFuncao";
 import Familia from "./components/Familia";
 import Membro from "./components/Membro";
 const elemento = document.getElementById('root')
 const jsx = <h1>Olá React!</h1>
 ReactDOM.render(
     <div>
-        <h1>Familia Lima</h1>
-        <Familia sobreNome="Lima">
-            <Membro nome="Marcos" />
-            <Membro nome="Mateus"/>
-            <Membro nome="Maria"/>
-            <Membro nome="Felipe"/>
-        </Familia>
+        <Funcao/>
     </div>
     ,elemento)
